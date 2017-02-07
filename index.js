338a71d84da9a2192c86b8b87e1db679601a80dd
Megan Coyle <mcoyle@usgbc.org>
2017-02-07 09:49:09 -0500

    Add OAuth

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+var url = require('url');
 var express = require('express');
+var authenticator = require('./authenticator');
+var config = require('./config');
 var app = express();
 
-app.get("/", function(req, res) {
-  res.send("you gots it");
+// Add cookie parsing functionality to Express app
+app.use(require('cookie-parser')());
+
+// Take user to Twitter's login page
+app.get('/auth/twitter', authenticator.redirectToTwitterLoginPage);
+
+// Callback url that the user is redirected to after signing in
+
+app.get(url.parse(config.oauth_callback).path, function(req, res) {
+  authenticator.authenticate(req, res, function(err) {
+    if (err) {
+      console.log(err);
+      res.sendStatus(401);
+    } else {
+      res.send("Authentication Successful");
+    }
+  });
 });
 
-app.listen(8080, function() {
-  console.log("Express server is running on port 8080");
+app.listen(config.port, function() {
+  console.log("Express server is running on port " + config.port);
 });
